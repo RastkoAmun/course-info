59d38d2e5d9eca2cf5be9d4d97068da7f9494b35
RastkoAmun <raciosh@gmail.com>
2023-02-01 09:23:08 -0800

    Adding functionality for greater number of courses

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,10 @@
 const Total = ({parts}) => {
-  // const total = total[0].exercises + total[1].exercises + total[2].exercises;
-  
-  const total = parts.reduce((sum, item) => sum + item.exercises, 0);
+  const total = parts.reduce((sum, item) => 
+    sum + item.exercises, 0);
   
   return(
     <div>
-      <p>Number of exercises: {total}</p>
+      <p className="total-number">Number of exercises: {total}</p>
     </div>
   )
 }
